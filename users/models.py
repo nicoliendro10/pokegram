9ab814245df2a93a5d38bd366d9e738d698c9e6b
nicoliendro10 <nick.sfra.7@gmail.com>
2021-11-25 00:33:50 -0300

    Add signup feature. Change email to unique

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Profile(models.Model):
-
+    User._meta.get_field('email')._unique = True
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     website = models.URLField(blank=True)
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from users.models import Profile
 
-
+from django.db.utils import IntegrityError
 
 def login_view(request):
     if request.user.is_authenticated:
@@ -25,3 +27,31 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        if password != password_confirmation:
+            message_error = {
+                'error': 'Password and password confirmation does not match'
+            }
+            return render(request, 'users/signup.html', message_error)
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            if User.objects.filter(email=user.email):
+                return render(request, 'users/signup.html', {'error': 'Email is already in used!'})
+        except IntegrityError:
+            message_error = {
+                'error': 'Username is already taken'
+            }
+            return render(request, 'users/signup.html', message_error)
+        user.email = email
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+    return render(request, 'users/signup.html')
