623b1bbf980c5fe119e7fc6de82c58840e587f1b
nicoliendro10 <nick.sfra.7@gmail.com>
2021-11-28 19:58:56 -0300

    Add create post view and template

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 import requests
 import json
 
+from posts.forms import PostForm
+from posts.models import Post
 from django.shortcuts import render
 
-@login_required
-def list_posts(request):
+
+def generate_pokemons():
     pokemons_list = []
     pokemons_request = requests.get("https://pokeapi.co/api/v2/pokemon?limit=5")
     pokemons_dict = json.loads(pokemons_request.content)
@@ -22,5 +25,27 @@ def list_posts(request):
     return render(request, 'posts/feed.html', {'pokemons_list': pokemons_list})
 
 @login_required
+def list_posts(request):
+    posts = Post.objects.all().order_by('-created')
+
+    return render(request, 'posts/feed.html', {'posts': posts})
+
+@login_required
 def create_post(request):
-    pass
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+    else:
+        form = PostForm()
+    
+    return render(
+        request,
+        'posts/new.html',
+        context={
+            'form': form,
+            'user': request.user,
+            'profile': request.user.profile
+        }
+    )
